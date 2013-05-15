7b7fe5995092d34b798d89a70cb4ebeba38e11a4
Aymeric <aymeric.laleau@gmail.com>
2013-05-15 08:44:45 +0200

    ajout de la librairie html2pdf_v4.03
    Création de la page pdf

---
+++ Projet fin d'année/codeAppliFrais/test.php
@@ -0,0 +1,12 @@
+<?php
+
+     session_start();
+     $content=$_SESSION['content'];
+
+    
+   
+    require_once('html2pdf_v4.03/html2pdf.class.php');
+    $html2pdf = new HTML2PDF('P','A4','fr');
+    $html2pdf->WriteHTML($content);
+    $html2pdf->Output('exemple.pdf');
+?>
