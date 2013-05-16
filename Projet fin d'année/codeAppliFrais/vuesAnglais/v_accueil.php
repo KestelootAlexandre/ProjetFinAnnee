5f371c3f5cb22d27f72e67b502a79e3e279234f6
Kesteloot Alexandre <kesteloot.alexandre@gmail.com>
2013-05-16 15:03:54 +0200

    Traduction en Anglais
    You're a funny guy

---
+++ Projet fin d'année/codeAppliFrais/vuesAnglais/v_accueil.php
@@ -0,0 +1,3 @@
+﻿<div id="accueil">
+Management of expenses
+</div>
---
+++ Projet fin d'année/codeAppliFrais/vuesAnglais/v_etatFrais.php
@@ -0,0 +1,74 @@
+﻿
+<h3>Drive expenses of the month <?php echo $numMois."-".$numAnnee?> : 
+    </h3>
+    <div class="encadre">
+    <p>
+        State : <?php echo $libEtat?> since <?php echo $dateModif?> <br> validated amount : <?php echo $montantValide?>
+              
+                     
+    </p>
+  	<table class="listeLegere">
+  	   <caption>Forfaitized elements </caption>
+        <tr>
+         <?php
+         foreach ( $lesFraisForfait as $unFraisForfait ) 
+		 {
+			$libelle = $unFraisForfait['libelle'];
+		?>	
+			<th> <?php echo $libelle?></th>
+		 <?php
+        }
+		?>
+		</tr>
+        <tr>
+        <?php
+          foreach (  $lesFraisForfait as $unFraisForfait  ) 
+		  {
+				$quantite = $unFraisForfait['quantite'];
+		?>
+                <td class="qteForfait"><?php echo $quantite?> </td>
+		 <?php
+          }
+		?>
+		</tr>
+    </table>
+  	<table class="listeLegere">
+  	   <caption>Description of the elements except fixed price -<?php echo $nbJustificatifs ?> received documents in proof -
+       </caption>
+             <tr>
+                <th class="date">Date</th>
+                <th class="libelle">Wording</th>
+                <th class='montant'>Total</th>                
+             </tr>
+        <?php      
+          foreach ( $lesFraisHorsForfait as $unFraisHorsForfait ) 
+		  {
+			$date = $unFraisHorsForfait['date'];
+			$libelle = $unFraisHorsForfait['libelle'];
+			$montant = $unFraisHorsForfait['montant'];
+		?>
+             <tr>
+                <td><?php echo $date ?></td>
+                <td><?php echo $libelle ?></td>
+                <td><?php echo $montant ?></td>
+             </tr>
+        <?php 
+          }
+		?>
+    </table>
+  </div>
+  </div>
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
